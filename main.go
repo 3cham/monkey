da0e96f2e3b8b6f0e74783bdecd36f89a07cd463
Tung Dang <thanhtung.nov@gmail.com>
2023-01-03 07:29:54 +0100

    add REPL for Monkey

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"monkey/internal/repl"
+	"os"
+	user2 "os/user"
+)
+
 func main() {
+	user, err := user2.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
 }
