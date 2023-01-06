69019b4642284a0f6e41eeac869efb580bb42839
Tung Dang <thanhtung.nov@gmail.com>
2023-01-06 07:55:44 +0100

    fix unittest by adding case for SEMICOLON in parseStatement()

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -81,6 +81,8 @@ func (p *Parser) parseStatement() ast.Statement {
 		return p.parseLetStatement()
 	case token.RETURN:
 		return p.parseReturnStatement()
+	case token.SEMICOLON:
+		return nil
 	default:
 		return p.parseExpressionStatement()
 	}
