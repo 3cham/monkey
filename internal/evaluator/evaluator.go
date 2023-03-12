80e4c2cdd7a62f89de00503327950f6533dde5c5
Tung Dang <thanhtung.nov@gmail.com>
2023-03-12 15:46:32 +0100

    evaluate infix expression for integer & boolean, evaluate if expression

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -21,6 +21,21 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		condition := Eval(node.Condition)
+		if isTruthy(condition) {
+			return Eval(node.Consequence)
+		} else if node.Alternative != nil {
+			return Eval(node.Alternative)
+		} else {
+			return NULL
+		}
 
 	// Expressions
 	case *ast.IntegerLiteral:
@@ -30,6 +45,68 @@ func Eval(node ast.Node) object.Object {
 	}
 
 	return NULL
+}
+
+func isTruthy(condition object.Object) bool {
+	switch condition {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
+}
+
+func evalInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.BOOLEAN_OBJ && right.Type() == object.BOOLEAN_OBJ:
+		return evalBooleanInfixExpression(operator, left, right)
+	default:
+		return NULL
+	}
+}
+
+func evalBooleanInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	leftEval := left.(*object.Boolean).Value
+	rightEval := right.(*object.Boolean).Value
+	switch operator {
+	case "==":
+		return nativeBoolean(leftEval == rightEval)
+	case "!=":
+		return nativeBoolean(leftEval != rightEval)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	leftEval := left.(*object.Integer).Value
+	rightEval := right.(*object.Integer).Value
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftEval + rightEval}
+	case "-":
+		return &object.Integer{Value: leftEval - rightEval}
+	case "*":
+		return &object.Integer{Value: leftEval * rightEval}
+	case "/":
+		return &object.Integer{Value: leftEval / rightEval}
+	case "<":
+		return nativeBoolean(leftEval < rightEval)
+	case ">":
+		return nativeBoolean(leftEval > rightEval)
+	case "==":
+		return nativeBoolean(leftEval == rightEval)
+	case "!=":
+		return nativeBoolean(leftEval != rightEval)
+	default:
+		return NULL
+	}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
--- internal/evaluator/evaluator_test.go
+++ internal/evaluator/evaluator_test.go
@@ -16,6 +16,15 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"10", 10},
 		{"-5", -5},
 		{"-10", -10},
+		{"5 + 5 + 5", 15},
+		{"5 * 2 * 5", 50},
+		{"5 * (2 + 5)", 35},
+		{"5 * 2 + 5", 15},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	for _, tt := range tests {
@@ -53,6 +62,23 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
 	}
 
 	for _, tt := range tests {
@@ -91,4 +117,37 @@ func TestBangOperator(t *testing.T) {
 		evaluated := testEval(tt.input)
 		testBooleanObject(t, evaluated, tt.expected)
 	}
+}
+
+func TestIfElseExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10 }", 10},
+		{"if (1 > 2) { 10 }", nil},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
+func testNullObject(t *testing.T, evaluated object.Object) bool {
+	if evaluated != NULL {
+		t.Errorf("object is not NULL. got=%T, (%+v)\n", evaluated, evaluated)
+		return false
+	}
+	return true
 }
